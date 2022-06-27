57f71da112e760dd2516072352ae2d366db0acea
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-06-28 00:32:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'New-Botz'
+  var sticker_name = 'MR_PRABOWO'
   var sticker_author = 'PICKFORD'
 } else {
   var sticker_name = stickerpack.spackname
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'NEW-BOTZ'
+global.wm = '© PICKFORD'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
