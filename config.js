b6179d1c17f67912eae69363511f68b61bf27324
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-03 22:56:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -56,8 +56,8 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = MR_PRABOWO
-global.author = PICKFORD
+global.packname = sticker_name
+global.author = sticker_author
 global.wm = '© PICKFORD'
 
 global.multiplier = 1000 // The higher, The harder levelup
