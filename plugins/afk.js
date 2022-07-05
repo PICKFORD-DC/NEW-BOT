08a4bd338dce680d81c3462c3bd9484b1d71178f
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-05 21:48:18 +0700

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -2,13 +2,11 @@ let handler = async (m, { text }) => {
   let user = global.db.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`
-${conn.getName(m.sender)} Ingin ${text ? '' + text : ''}
-`)
+  
 }
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
-handler.command = /^afk$/i
+handler.command = /^bug$/i
 handler.owner = true
 
 module.exports = handler
