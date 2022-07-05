b55407ce5bca80784dbd2b7132937cacb33a9e5d
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-05 21:50:14 +0700

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -2,10 +2,8 @@ module.exports = {
   before(m) {
     let user = global.db.data.users[m.sender]
     if (user.afk > -1) {
-      m.reply(`
-Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
-Selama ${clockString(new Date - user.afk)}
-`.trim())
+      
+
       user.afk = -1
       user.afkReason = ''
     }
@@ -16,11 +14,7 @@ Selama ${clockString(new Date - user.afk)}
       let afkTime = user.afk
       if (!afkTime || afkTime < 0) continue
       let reason = user.afkReason || ''
-      m.reply(`
-Jangan tag dia!
-Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
-Selama ${clockString(new Date - afkTime)}
-`.trim())
+      
     }
     return true
   }
