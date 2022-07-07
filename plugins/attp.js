d8b08cbfbd40c44072bee11586bcb23c2caf3973
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-07 13:10:03 +0700

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  conn.sendMedia(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+  conn.sendFile(m.chat, global.API('xteam.xyz', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
