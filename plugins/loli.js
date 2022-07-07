77e54ccaa388dcc4528af14fa5615aa48d038a85
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-07 08:50:03 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -2,7 +2,7 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	scrap.pinterest("loli","loli kawai","loli sagiri","anime loli","loli cat").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
+	scrap.pinterest("loli","loli kawai","loli sagiri","anime loli","loli cat").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"janji gak jadi bahan coli 😂",m))
    }
     
 handler.help = ['loli']
