839559d6f613d8e4f6362a4cfcd7eb142e56ea11
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-08 23:56:35 +0700

    Update jajal.js

--- plugins/jajal.js
+++ plugins/jajal.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
-  let res = await fetch('https://telegra.ph/file/6cea8b0f5d44e5796a9d8.jpg')
-  if (!res.ok) throw await res.text()
+
+if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'JAJAL DOANG BANG', m)
+  conn.sendFile(m.chat, json.url, 'https://telegra.ph/file/6cea8b0f5d44e5796a9d8.jpg', 'JAJAL DOANG BANG', m)
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
