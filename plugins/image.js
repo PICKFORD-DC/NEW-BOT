127e57429e1aa167529a92d761e5915d5450e1e0
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-09 04:43:36 +0700

    Update image.js

--- plugins/image.js
+++ plugins/image.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let helloaine = fs.readFileSync('./lib/IMG_20220115_120829_433.jpg')
-conn.sendFile(m.chat, helloaine, '', '', m, true)
+conn.sendFile(m.chat, helloaine, '', 'aku lek jajal ongab', m, true)
 }
 
 handler.customPrefix = /^(lolot)$/i
