4ab0888ebcfae3f0a74d707c95808a1731e9ada3
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-09 14:30:42 +0700

    Update image.js

--- plugins/image.js
+++ plugins/image.js
@@ -1,7 +1,21 @@
 let fs = require('fs')
 let handler = async (m) => {
 let helloaine = fs.readFileSync('./lib/20220709_140748.jpg')
-conn.sendFile(m.chat, helloaine, '', 'aku lek jajal ongab', m, true)
+conn.sendFile(m.chat, helloaine, '', '
+*• SEWA BOT & UP TO PREMIUM •*
+        
+1. Grup / Selama Bot Aktif
+Rp. 5.000 Dana
+Rp. 5.000 Pulsa
+2. Premium / Selama Bot Aktif
+Rp. 3.000 Dana
+Rp. 3.000 Pulsa
+3. Premium + Grup / Selama Bot Aktif
+Rp. 8.000 Dana
+Rp. 8.000 Pulsa
+
+*Info Selanjutnya Hubungi Owner*
+', m, true)
 }
 
 handler.customPrefix = /^(p)$/i
