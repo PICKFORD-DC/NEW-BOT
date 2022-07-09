c8aabeba11e1092f3009bf47c7703e50f7cce1a6
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-10 04:45:12 +0700

    Update quoteshalal.js

--- plugins/quoteshalal.js
+++ plugins/quoteshalal.js
@@ -2,7 +2,7 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	scrap.pinterest("kata kata Ustadz Abdulsomad","kata kata islam").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Random Quotes",m))
+	scrap.pinterest("kata kata ustadz abdul somad","kata kata islam").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Random Quotes",m))
    }
     
 handler.help = ['quoteshalal']
