2832f33f6460dbb9c4a99915d26aab6fb6be116d
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-10 03:28:02 +0700

    Add files via upload

--- plugins/quoteshalal.js
+++ plugins/quoteshalal.js
@@ -2,11 +2,11 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	scrap.pinterest("Quotes Ustad Abdulsomad","quotes halal").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"janji gak jadi bahan coli 😂",m))
+	scrap.pinterest("Quotes Ustad Abdulsomad","quotes halal").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Random Quotes",m))
    }
     
 handler.help = ['quoteshalal']
-handler.tags = ['internet']
+handler.tags = ['quotes']
 handler.command = /^(quoteshalal)$/i
 handler.limit = true
 
