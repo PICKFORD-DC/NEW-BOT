c212717cb1625d5f643d7a3b8402b4646cacb3a6
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-09 14:56:19 +0700

    Update sewah.js

--- plugins/sewah.js
+++ plugins/sewah.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 let handler = async (m) => {
-let helloaine = fs.readFileSync('./lib/20220709_140748.jpg')
+let helloaine = fs.readFileSync('./lib/AW3884646_00.gif')
 conn.sendFile(m.chat, helloaine, '', '*• SEWA BOT & UP TO PREMIUM •*\n\n1. Grup / Selama Bot Aktif\nRp. 5.000 Dana\nRp. 5.000 Pulsa\n\n2. Premium / Selama Bot Aktif\nRp. 3.000 Dana\nRp. 3.000 Pulsa\n\n3. Premium + Grup / Selama Bot Aktif\nRp. 8.000 Dana\nRp. 8.000 Pulsa\n\n\nHubungi Owner Untuk Melanjutkan', m, true)
 }
 
