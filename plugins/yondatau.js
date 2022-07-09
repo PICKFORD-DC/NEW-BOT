049ef056d8bb7b59515d5f9a1ef183257e4fb2a8
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-10 03:54:08 +0700

    Update yondatau.js

--- plugins/yondatau.js
+++ plugins/yondatau.js
@@ -3,8 +3,10 @@ let handler = async (m) => {
 let helloaine = fs.readFileSync('./mp3/yondatau.opus')
 conn.sendFile(m.chat, helloaine, '', '', m, true)
 }
+handler.help = ['lagi apa?']
 
-handler.customPrefix = /^(lagiapa)$/i
+handler.tags = ['gabut']
+handler.customPrefix = /^(lagiapa|lagi apa?)$/i
 handler.command = new RegExp
 
 handler.limit = true
