fd0cd6f2a24869ae06e318b4cb8b258e3d67b861
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-09 14:58:12 +0700

    Update and rename plugins/sewah.js to plugin/sewah.js

--- plugin/sewah.js
+++ plugin/sewah.js
@@ -4,7 +4,7 @@ let helloaine = fs.readFileSync('./lib/AW3884646_00.gif')
 conn.sendFile(m.chat, helloaine, '', '*• SEWA BOT & UP TO PREMIUM •*\n\n1. Grup / Selama Bot Aktif\nRp. 5.000 Dana\nRp. 5.000 Pulsa\n\n2. Premium / Selama Bot Aktif\nRp. 3.000 Dana\nRp. 3.000 Pulsa\n\n3. Premium + Grup / Selama Bot Aktif\nRp. 8.000 Dana\nRp. 8.000 Pulsa\n\n\nHubungi Owner Untuk Melanjutkan', m, true)
 }
 
-handler.customPrefix = /^(swa)$/i
+handler.customPrefix = /^(sewa)$/i
 handler.command = new RegExp
 
 handler.limit = true
