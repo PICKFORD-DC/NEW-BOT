6c4b0d9b250cb557d9d8404246bf641c7fdba3e9
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-09 15:41:37 +0700

    Update sewah.js

--- plugins/sewah.js
+++ plugins/sewah.js
@@ -3,10 +3,6 @@ let handler = async (m) => {
 let helloaine = fs.readFileSync('./lib/20220709_145249.jpg')
 conn.sendFile(m.chat, helloaine, '', '*• SEWA BOT & UP TO PREMIUM •*\n\n1. Grup / Selama Bot Aktif\n\nRp. 5.000 Dana\nRp. 5.000 Pulsa\n\n2. Premium / Selama Bot Aktif\nRp. 3.000 Dana\nRp. 3.000 Pulsa\n\n3. Premium + Grup / Selama Bot Aktif\nRp. 8.000 Dana\nRp. 8.000 Pulsa\n\n\nHubungi Owner Untuk Melanjutkan', m, true)
 }
-conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-
 
 handler.customPrefix = /^(sewa)$/i
 handler.command = new RegExp
