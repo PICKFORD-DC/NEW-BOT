f2797ab5f8310699f88976f329e85002f9774fc6
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-07-17 11:35:14 +0700

    Update donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -1,13 +1,1 @@
-let fs = require('fs')
-let handler = async (m) => {
-let helloaine = fs.readFileSync('./lib/20220712_170633.jpg')
-conn.sendFile(m.chat, helloaine, '', '*• DONASI •*\n\n083806211924(DANA)\n\n083806211924(PULSA)\n\n\n\n MAKASIH BAGI YANG SUDAH BERSEDEKAH SEMOGA AMAL NYA DI TERIMA OLEH ALLAH SUBAHANAHUWATALA 🙏', m, true)
-}
 
-handler.customPrefix = /^(donasi)$/i
-handler.command = new RegExp
-
-handler.limit = true
-
-
-module.exports = handler
