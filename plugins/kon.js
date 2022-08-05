459b76a0ddc30b6a64d87470a5552072a6a38802
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-08-05 17:36:05 +0700

    Add files via upload

---
+++ plugins/kon.js
@@ -0,0 +1,56 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  let pepe = 'https://telegra.ph/file/2e2eb7d093fec08fd00d4.jpg'
+  let baper = await fetch(pepe).then(a => a.buffer())
+
+  let aine = '6289654360447@s.whatsapp.net'
+  let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://chat.whatsapp.com/GW9FGCanGtmAmokDHgBgvi")
+  let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://chat.whatsapp.com/GW9FGCanGtmAmokDHgBgvi")
+  let c = pickRandom([a, b])
+  let d = await fetch(c).then(a => a.buffer())
+  let prepare = await require('@adiwajshing/baileys').generateWAMessageFromContent(m.key.remoteJid,{listMessage:{
+  title: `${await conn.getName(conn.user.jid)}`,
+  description: ` *• SEWA BOT & UP TO PREMIUM •*
+        
+1. Grup / 30 Hari
+Rp. 15.000 Dana
+Rp. 15.000 Pulsa
+
+2. Premium / 30 Hari
+Rp. 10.000 Dana
+Rp. 10.000 Pulsa
+
+3. Premium + Grup / 30 Hari
+Rp. 20.000 Dana
+Rp. 20.000 Pulsa
+
+wa.me/${owner[0]}
+*Bukan Bot!!!*
+*Owner ${conn.user.name}*
+`,
+  buttonText: 'Harga Sesuai Pasaran',
+  listType: 2,
+  productListInfo: {
+  productSections: [{
+  title:'Klik untuk order',
+  products:[{productId:'GW9FGCanGtmAmokDHgBgvi'}]}],
+  headerImage: { productId: 'GW9FGCanGtmAmokDHgBgvi',
+  jpegThumbnail: baper },
+  businessOwnerJid: `6289654360447@s.whatsapp.net`
+  },
+  footerText: 'https://aiinne.github.io',
+  }},{})
+  conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+
+}
+handler.help = ['jajalya']
+handler.tags = ['main']
+handler.command = /^(jajalya)$/i
+
+module.exports = handler
+
+function pickRandom(list) {
+        return list[Math.floor(Math.random() * list.length)]
+    }
